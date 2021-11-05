1df6cd3ba158f6dd8d6e76423dadb41b56af97b9
RakibRahman11 <rahmanrakib207@gmail.com>
2021-11-06 02:32:07 +0600

    appoint post

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const port = process.env.DB_HOST || 5000
 const { MongoClient } = require('mongodb');
-
 require('dotenv').config()
+const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpttu.mongodb.net/${process.env.DB_USER}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -12,6 +15,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     await client.connect();
+    const database = client.db("doctors-portal");
+    const appointmentCollection = database.collection("appointments");
+
+    app.post('/appointments', async(req, res) => {
+      const appointment = req.body
+      const result = await appointmentCollection.insertOne(appointment);
+      res.json(result);
+    })
   } finally {
     // await client.close();
   }
@@ -19,9 +30,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Doctors portal!')
+  res.send('Doctors portal!')
 })
 
 app.listen(port, () => {
-    console.log(`App listening :${port}`)
+  console.log(`App listening :${port}`)
 })
