a16d1306f440a45d694fcac50f00d3092a3aa049
RakibRahman11 <rahmanrakib207@gmail.com>
2021-11-07 03:42:31 +0600

    main-project

--- index.js
+++ index.js
@@ -17,21 +17,57 @@ async function run() {
     await client.connect();
     const database = client.db("doctors-portal");
     const appointmentCollection = database.collection("appointments");
+    const usersCollection = database.collection("users");
 
-    app.get('/appointments', async(req, res) => {
+    app.get('/appointments', async (req, res) => {
       const email = req.query.email
       const date = new Date(req.query.date).toLocaleDateString()
-      const query = {email:email, date:date}
+      const query = { email: email, date: date }
       const cursor = appointmentCollection.find(query)
       const appointment = await cursor.toArray();
       res.send(appointment)
     })
 
-    app.post('/appointments', async(req, res) => {
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { email: email}
+      const user = await usersCollection.findOne(query)
+      let isAdmin = false
+      if(user?.role === 'admin'){
+        isAdmin = true
+      }
+      res.json({admin: isAdmin``})
+    })
+
+    app.post('/appointments', async (req, res) => {
       const appointment = req.body
       const result = await appointmentCollection.insertOne(appointment);
       res.json(result);
     })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    })
+
+    app.put('/users', async (req, res) => {
+      const user = req.body
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user};
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    })
+
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body
+      const filter = { email: user.email };
+      const updateDoc = { $set: {role:'admin'}};
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    })
+
   } finally {
     // await client.close();
   }
